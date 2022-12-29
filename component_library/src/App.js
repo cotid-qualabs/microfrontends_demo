8235f01a19e59a278e333b61b69e60172edc58ad
constanza-dibueno <constanzad@qualabs.com>
2022-12-29 10:46:04 -0300

    init

---
+++ component_library/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Component Library</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ remote_2/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Remote 2</h1>
+    </div>
+  );
+}
+
+export default App;
