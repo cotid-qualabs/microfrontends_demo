f338ed1db1c810ac50ff653e274cee10d451c6e5
constanza-dibueno <constanzad@qualabs.com>
2023-01-03 14:07:05 -0300

    component library npm package implementation

--- remote_1/src/App.js
+++ remote_1/src/App.js
@@ -1,7 +1,10 @@
+import {Button} from 'qnd_component_library_demo';
+
 function App() {
   return (
     <div className="App">
      <h1>Remote 1</h1>
+     <Button text="Component Library Btn 1" classname="btn btn-primary"/>
     </div>
   );
 }
--- remote_2/src/App.js
+++ remote_2/src/App.js
@@ -1,7 +1,12 @@
+import {Button} from 'qnd_component_library_demo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 function App() {
   return (
     <div className="App">
       <h1>Remote 2</h1>
+      <Button text="Component Library Btn 2" classname="btn btn-primary"/>
     </div>
   );
 }
